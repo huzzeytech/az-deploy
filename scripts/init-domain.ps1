4afefc3472b5370863f0d34c908cd673937d19f4
Cody Hussey <3787405e@opayq.com>
2019-04-12 08:35:22 -0400

    noreboot

--- scripts/init-domain.ps1
+++ scripts/init-domain.ps1
@@ -1,4 +1,4 @@
 $CustomDomain = $Args[0] + "-yubi.fun"
 
 Install-WindowsFeature -name AD-Domain-Services, rsat-adds -IncludeAllSubFeature
-Install-ADDSForest -DomainName $CustomDomain -InstallDNS:$true -safemodeadministratorpassword (convertto-securestring "Testing123!" -asplaintext -force) -NoRebootOnCompletion
+Install-ADDSForest -DomainName $CustomDomain -InstallDNS:$true -safemodeadministratorpassword (convertto-securestring "Testing123!" -asplaintext -force) -NoRebootOnCompletion:$true
