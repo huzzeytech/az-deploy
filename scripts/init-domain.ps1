3607778f5ea7f488ae97bf44a0fb838461ecb01e
Cody Hussey <3787405e@opayq.com>
2019-04-12 14:17:34 -0400

    add force to deploy domain with restart

--- scripts/init-domain.ps1
+++ scripts/init-domain.ps1
@@ -4,4 +4,4 @@ param (
 
 $CustomDomain = $Args[0] + "-yubi.fun"
 Install-WindowsFeature -name AD-Domain-Services, rsat-adds -IncludeAllSubFeature
-Install-ADDSForest -DomainName $CustomDomain -InstallDNS:$true -safemodeadministratorpassword (convertto-securestring "Testing123!" -asplaintext -force) #-NoRebootOnCompletion:$true
+Install-ADDSForest -DomainName $CustomDomain -InstallDNS:$true -safemodeadministratorpassword (convertto-securestring "Testing123!" -asplaintext -force) -Force #-NoRebootOnCompletion:$true
