a156e1500f2183f71381bb547497aab29ae63c60
Cody Hussey <3787405e@opayq.com>
2019-04-12 08:59:23 -0400

    added script with parameter variables

--- scripts/init-domain.ps1
+++ scripts/init-domain.ps1
@@ -1,4 +1,7 @@
-$CustomDomain = $Args[0] + "-yubi.fun"
+param (
+    [string]$CustomDomain
+)
 
+$CustomDomain = $Args[0] + "-yubi.fun"
 Install-WindowsFeature -name AD-Domain-Services, rsat-adds -IncludeAllSubFeature
 Install-ADDSForest -DomainName $CustomDomain -InstallDNS:$true -safemodeadministratorpassword (convertto-securestring "Testing123!" -asplaintext -force) -NoRebootOnCompletion:$true
