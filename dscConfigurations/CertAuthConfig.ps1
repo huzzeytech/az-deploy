13d91c977b2800960410dcd7c2f9bbd80c15f55c
Cody Hussey <3787405e@opayq.com>
2019-05-29 15:15:40 -0400

    Remove Credential Name from ConfigData

--- dscConfigurations/CertAuthConfig.ps1
+++ dscConfigurations/CertAuthConfig.ps1
@@ -5,7 +5,7 @@ Configuration CertAuthConfig
         $credname
     ) #>
 
-    $domainCredential = Get-AutomationPSCredential -Name yubi37-yubi
+    $domainCredential = Get-AutomationPSCredential 'yubi37-yubi'
 
     #Write-Verbose $domainCredential
     Import-DscResource -ModuleName ActiveDirectoryCSDsc
